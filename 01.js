e42d358072f4bdc7abd7b196549f83396c29a30d
BrunoSprint <brunosprintdev@outlook.com>
2023-12-20 21:13:32 -0300

    .

--- 01.js
+++ 01.js
@@ -1,2 +1,18 @@
+// Variáveis, Vetores ou Matrizes, servem para armazenar dados.
 
-let JavaScript
+let carro = ["ford", "chevrolet", "honda"]
+
+let teammarks =[
+    ["ford", "v8", "level 5"]
+    ["chevrolet", "v8" "level 5"]
+    ["honda", "v6-Turbo" "level 5"]
+]
+
+console.log(carro[0][1])
+
+// Neste caso é feita a seleção da linha 0 e coluna 1, a saida será v8.
+
+
+console.log("hello");
+
+//run ctrl+alt+n
