84b0e0f09e1f2efbdb6f1cb8381482a61337db6f
BrunoSprint <brunosprintdev@outlook.com>
2024-01-05 21:40:03 -0300

    .

--- variaveis_operadores.js
+++ variaveis_operadores.js
@@ -13,7 +13,30 @@ console.log(pi)
 
 
 // Simbolos ultilizados para definir operadores aritiméticos:
-//. + . - . * . / ."mais-menos-vezes-dividir"
+//. + . - . * . / . % ."mais-menos-vezes-dividir-porcentagem"
  
 
 
+// Operadores Lógicos: soma+ | subtração- | multiplicação * | divisão / | porcentagem % |
+
+
+
+// exemplo soma
+let idade = 30
+console.log(idade)
+idade = 30 + 6
+console.log("Valor da Variavel-" + idade)   // neste ex: mostra a concatenação de texto + variavel por intermedio do sinal de soma +
+
+
+
+// exemplo subtração
+
+idade = 36 - 3
+console.log(idade)
+
+
+// exemplo multiplicação
+
+
+
+//pausa aula trabalhando com operadores aritimeticos, de logica de programação 7.05
