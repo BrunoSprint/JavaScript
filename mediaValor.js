bab169d30e32f53efcb13ec554704f4adcd77943
BrunoSprint <117302910+BrunoSprint@users.noreply.github.com>
2024-03-04 17:20:10 -0300

    .

--- mediaValor.js
+++ mediaValor.js
@@ -1,3 +1,14 @@
+/*
+-  Faça um algoritimo que dado 3 valores, calculam a média geral e classifica: Reprovação- Recuperação-Média Aceita.
+média = (nota1 + nota2 + nota3)   / 3;
+
+Classificação:
+- Média menor que 5, reprovação;
+- Média entre 5 e 7, recuperação;
+- Média acima de 7, média aceita;
+
+*/
+
 const valor1 = 6.5;
 const valor2 = 7;
 const valor3 = 8;
