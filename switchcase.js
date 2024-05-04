0cce42bdf7d6bf6f83158faeb32ee2b62d27fde9
Bruno César <117302910+BrunoSprint@users.noreply.github.com>
2024-05-04 13:32:49 -0300

    Create switchcase.js

---
+++ switchcase.js
@@ -0,0 +1,13 @@
+switch(opcao) {
+    case 1:
+        // código a ser executado se opcao for igual a 1
+        break;
+    case 2:
+        // código a ser executado se opcao for igual a 2
+        break;
+    case 3:
+        // código a ser executado se opcao for igual a 3
+        break;
+    default:
+        // código a ser executado se opcao não corresponder a nenhum dos casos anteriores
+}
